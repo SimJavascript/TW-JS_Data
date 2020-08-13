433ced8629af3cf5322dec881440fead3b0383df
simonduquaine <duquaine.simon@gmail.com>
2020-08-13 11:04:09 +0200

    ok

--- assets/graph1.js
+++ assets/graph1.js
@@ -1,80 +1,97 @@
+class DataFromTable {
+    constructor() {
+
+    }
+
+    getData() {
+
+    }
+
+    getCountry() {
+        let country = [];
+        for (let i = 2; i < allData.length; i++) {
+            country.push(allData[i][1]);
+        }
+        return country;
+    }
+
+    getCrimes() {
+
+    }
+
+    getYears() {
+
+    }
+}
+
 // get year
 let table1 = document.getElementById("table1");
 let allData = [...table1.rows].map(t => [...t.children].map(u => u.innerText));
 let years = allData[1].filter(function (el) { return el; });
-let dataBelgium = allData[2];
-let dataBulgaria = allData[3];
 
-function getCountry() {
-    let country = [];
-    for (let i = 2; i < allData.length; i++) {
-        country.push(allData[i][1]);
-        // for (let j = 1; j < country.countrylength; j++) {
-        //     console.log(country[j]);
-
-        // }
-
-    }
-    console.log(country);
-
+let country = [];
+for (let i = 2; i < allData.length; i++) {
+    country.push(allData[i][1]);
 }
 
-
-let belgiumCrimes = [];
-for (let i = 2; i < dataBelgium.length; i++) {
-    belgiumCrimes.push(parseFloat(dataBelgium[i]));
+let crimes = [];
+for (let i = 2; i < allData.length; i++) {
+    crimes.push(allData[i].slice(2));
+}
+let crimesInt = [];
+for (let i = 0; i < crimes.length; i++) {
+    crimesInt.push(crimes.map(new Number));
 }
 
-let crimes2 = [];
-for (let i = 2; i < dataBulgaria.length; i++) {
-    crimes2.push(parseFloat(dataBulgaria[i]))
-    
-}
+console.log(crimesInt);
+console.log(crimes);
+
+
 
 let labels = years;
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: years,
-      datasets: [{
-        label: "My First dataset",
-        data: belgiumCrimes,
-        fill: false,
-      }, {
-        label: "My Second dataset",
-        fill: false,
-        data: crimes2,
-      }]
+        labels: years,
+        datasets: [{
+            label: country[0],
+            data: crimes[0],
+            fill: false,
+        }, {
+            label: country[1],
+            fill: false,
+            data: crimes[1],
+        }]
     },
     options: {
-      responsive: true,
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart'
-      },
-      tooltips: {
-        mode: 'label',
-      },
-      hover: {
-        mode: 'nearest',
-        intersect: true
-      },
-      scales: {
-        xAxes: [{
-          display: true,
-          scaleLabel: {
+        responsive: true,
+        title: {
             display: true,
-            labelString: 'Month'
-          }
-        }],
-        yAxes: [{
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: 'Value'
-          }
-        }]
-      }
+            text: 'Chart.js Line Chart'
+        },
+        tooltips: {
+            mode: 'label',
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        }
     }
-  });    
+});    
