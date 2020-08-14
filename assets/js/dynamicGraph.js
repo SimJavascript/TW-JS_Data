e6e3be68a846e7c07565b247546f6e68f6af3f15
simonduquaine <duquaine.simon@gmail.com>
2020-08-14 10:06:59 +0200

    final dynamic graph

--- assets/js/dynamicGraph.js
+++ assets/js/dynamicGraph.js
@@ -1,15 +1,21 @@
+const divChart = document.createElement("div");
+divChart.setAttribute("id", "chartContainer");
+divChart.setAttribute("style", "height: 300px; width: 100%;")
+const title = document.getElementById("firstHeading");
+title.after(divChart);
+
 var dataPoints = [];
-$.getJSON("https://canvasjs.com/services/data/datapoints.php", function(data) {  
-    $.each(data, function(key, value){
-        dataPoints.push({x: value[0], y: parseInt(value[1])});
+$.getJSON("https://canvasjs.com/services/data/datapoints.php", function (data) {
+    $.each(data, function (key, value) {
+        dataPoints.push({ x: value[0], y: parseInt(value[1]) });
     });
-    chart = new CanvasJS.Chart("chartContainer",{
-        title:{
-            text:"Live Chart with dataPoints from External JSON"
+    chart = new CanvasJS.Chart("chartContainer", {
+        title: {
+            text: "Live Chart with dataPoints from External JSON"
         },
         data: [{
-        type: "line",
-        dataPoints : dataPoints,
+            type: "line",
+            dataPoints: dataPoints,
         }]
     });
     chart.render();
@@ -17,14 +23,14 @@ $.getJSON("https://canvasjs.com/services/data/datapoints.php", function(data) {
 });
 
 function updateChart() {
-    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function(data) {
-        $.each(data, function(key, value) {
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function (data) {
+        $.each(data, function (key, value) {
             dataPoints.push({
                 x: parseInt(value[0]),
                 y: parseInt(value[1])
             });
-       });
-       chart.render();
-       setTimeout(function(){updateChart()}, 1000);
+        });
+        chart.render();
+        setTimeout(function () { updateChart() }, 1000);
     });
- }
+}
